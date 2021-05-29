f05e810cc75791176db8b72f769e3b210c6c418c
smetzdev <metz@raum3.de>
2021-05-30 00:08:26 +0200

    feat(page): add intial front-page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,59 @@
 import "twin.macro"
+import { StaticImage } from "gatsby-plugin-image"
 import { Layout, SEO } from "../components"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="Hello" />
+    <div tw="w-screen h-screen flex flex-col items-center justify-center text-center">
+      <h1>
+        <StaticImage
+          tw="w-24 h-auto rounded-full"
+          src="../images/smetzdev.jpg"
+          alt="@smetzdev"
+        />
+      </h1>
+      <ul tw="flex space-x-6 mt-4 text-gray-500">
+        <li>
+          <a
+            tw="hover:text-gray-800 transition-colors"
+            href="mailto:hello@smetz.dev"
+          >
+            Mail
+          </a>
+        </li>
+        <li>
+          <a
+            tw="hover:text-gray-800 transition-colors"
+            href="https://twitter.com/smetzdev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+        </li>
+        <li>
+          <a
+            tw="hover:text-gray-800 transition-colors"
+            href="https://github.com/smetzdev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a
+            tw="hover:text-gray-800 transition-colors"
+            href="https://dev.to/smetzdev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Dev.to
+          </a>
+        </li>
+      </ul>
+    </div>
   </Layout>
 )
 
