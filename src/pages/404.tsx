c3916a59b4c7ecb5720cac226fcc8ea309144824
smetzdev <metz@raum3.de>
2021-06-11 00:22:26 +0200

    feat(pages/index + pages/404): update to new designsystem / add gravatar

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,22 +1,18 @@
 import { Link } from "gatsby"
 import "twin.macro"
-import { StaticImage } from "gatsby-plugin-image"
 import { Layout, SEO } from "../components"
+import { Gravatar } from "../gatsby-components"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="404 - Page not found" />
-    <div tw="w-screen h-screen flex flex-col items-center justify-center text-center bg-gradient-to-br from-gray-50 to-white">
+    <div tw="w-screen h-screen flex flex-col items-center justify-center text-center bg-gray-900 text-gray-300">
       <h1>
-        <StaticImage
-          tw="w-24 h-auto rounded-full"
-          src="../images/smetzdev.jpg"
-          alt="@smetzdev"
-        />
+        <Gravatar />
       </h1>
-      <p tw="text-gray-500 mt-4">
+      <p tw="mt-4">
         Page not found...{" "}
-        <Link tw="hover:text-gray-800 transition-colors underline" to="/">
+        <Link tw="hover:text-primary transition-colors underline" to="/">
           Go to Homepage
         </Link>
       </p>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,70 +1,53 @@
 import "twin.macro"
 import { StaticImage } from "gatsby-plugin-image"
 import { Layout, SEO } from "../components"
+import { Gravatar } from "../gatsby-components"
+
+// https://de.gravatar.com/site/implement/images/
+const gravatarURL =
+  "https://www.gravatar.com/avatar/fb6a9efd248314aad6fece7135ecfe0d?size=500"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Hello" />
-    <div tw="w-screen h-screen flex flex-col items-center justify-center text-center bg-gradient-to-br from-gray-50 to-white">
+    <div tw="w-screen h-screen flex flex-col items-center justify-center text-center bg-gray-900 text-white">
       <h1>
-        <StaticImage
-          tw="w-24 h-auto rounded-full"
-          src="../images/smetzdev.jpg"
-          alt="@smetzdev"
-        />
+        <Gravatar />
       </h1>
-      <ul tw="flex space-x-6 mt-4 text-gray-500">
-        <li>
-          <a
-            tw="hover:text-gray-800 transition-colors"
-            href="mailto:hello@smetz.dev"
-          >
-            Mail
-          </a>
-        </li>
-        <li>
-          <a
-            tw="hover:text-gray-800 transition-colors"
-            href="https://github.com/smetzdev"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
-        </li>
-        <li>
-          <a
-            tw="hover:text-gray-800 transition-colors"
-            href="https://twitter.com/smetzdev"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Twitter
-          </a>
-        </li>
-        <li>
-          <a
-            tw="hover:text-gray-800 transition-colors"
-            href="https://www.instagram.com/smetzdev/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Insta
-          </a>
-        </li>
-        <li>
-          <a
-            tw="hover:text-gray-800 transition-colors"
-            href="https://dev.to/smetzdev"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dev.to
-          </a>
-        </li>
+      <ul tw="flex space-x-6 mt-4 text-gray-300">
+        {links.map(link => (
+          <li key={link.title.toLowerCase()}>
+            <a tw="hover:text-primary transition-colors" href={link.href}>
+              {link.title}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   </Layout>
 )
+
+const links = [
+  {
+    title: "Mail",
+    href: "mailto:hello@smetz.dev",
+  },
+  {
+    title: "GitHub",
+    href: "https://github.com/smetzdev",
+  },
+  {
+    title: "Twitter",
+    href: "https://twitter.com/smetzdev",
+  },
+  {
+    title: "Dev.to",
+    href: "https://dev.to/smetzdev",
+  },
+  {
+    title: "Insta",
+    href: "https://www.instagram.com/smetzdev/",
+  },
+]
 
 export default IndexPage
