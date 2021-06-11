4a25fc72f220325a05dfa060694280a00a70e731
smetzdev <metz@raum3.de>
2021-06-11 10:00:55 +0200

    feat(global-styles): add global background-color and text-color

---
+++ src/styles/global-styles/_base-styles.ts
@@ -0,0 +1,7 @@
+import tw, { css } from "twin.macro"
+
+export const baseStyles = css`
+  body {
+    ${tw`text-gray-300 bg-gray-900`}
+  }
+`
--- src/styles/global-styles/global-styles.tsx
+++ src/styles/global-styles/global-styles.tsx
@@ -1,9 +1,12 @@
 // @ts-ignore
 import React from "react"
 import { GlobalStyles as TwinMacroGlobalStyles } from "twin.macro"
+import { Global } from "@emotion/react"
+import { baseStyles } from "./_base-styles"
 
 export const GlobalStyles = () => (
   <>
     <TwinMacroGlobalStyles />
+    <Global styles={baseStyles} />
   </>
 )
