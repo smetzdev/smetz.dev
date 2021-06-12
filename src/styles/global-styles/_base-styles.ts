5a08cb15d4c983eee4f29dac6279afa7a6d9b310
smetzdev <metz@raum3.de>
2021-06-12 15:39:42 +0200

    feat(global-styles): add selction color

--- src/styles/global-styles/_base-styles.ts
+++ src/styles/global-styles/_base-styles.ts
@@ -4,4 +4,11 @@ export const baseStyles = css`
   body {
     ${tw`text-gray-300 bg-gray-900`}
   }
+  *::-moz-selection {
+    ${tw`text-gray-900 bg-primary`}
+  }
+
+  ::selection {
+    ${tw`text-gray-900 bg-primary`}
+  }
 `
