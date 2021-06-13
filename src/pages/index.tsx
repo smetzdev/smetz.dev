e0c093113c4b961a656537173906bd58968b2b1d
smetzdev <metz@raum3.de>
2021-06-13 23:31:28 +0200

    feat(pages/index): add initial design-system to index-page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,18 @@
 import "twin.macro"
 import { Gravatar, Layout, SEO } from "../gatsby-components"
+import { Intro } from "../components"
+import IntroStories from "../components/intro/intro.stories"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Hello" />
-    <div tw="w-screen h-screen flex flex-col items-center justify-center text-center">
-      <h1>
-        <Gravatar tw="w-32 h-auto" />
-      </h1>
-      <ul tw="flex space-x-6 mt-4">
-        {links.map(link => (
-          <li key={link.title.toLowerCase()}>
-            <a tw="hover:text-primary transition-colors" href={link.href}>
-              {link.title}
-            </a>
-          </li>
-        ))}
-      </ul>
+    <div tw="w-screen h-screen flex items-center">
+      <Intro
+        Image={<Gravatar tw="w-full h-full" />}
+        socialMediaLinks={IntroStories.args.socialMediaLinks}
+      />
     </div>
   </Layout>
 )
-
-const links = [
-  {
-    title: "Mail",
-    href: "mailto:hello@smetz.dev",
-  },
-  {
-    title: "GitHub",
-    href: "https://github.com/smetzdev",
-  },
-  {
-    title: "Twitter",
-    href: "https://twitter.com/smetzdev",
-  },
-  {
-    title: "Dev.to",
-    href: "https://dev.to/smetzdev",
-  },
-  {
-    title: "Insta",
-    href: "https://www.instagram.com/smetzdev/",
-  },
-]
 
 export default IndexPage
