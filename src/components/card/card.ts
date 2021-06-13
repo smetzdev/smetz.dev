c6f78671b3cbfed90e6b8735febfa782faaf4176
smetzdev <metz@raum3.de>
2021-06-13 21:39:00 +0200

    feat(components/card): add card props / card-content-footer full-width

--- src/components/card/card.ts
+++ src/components/card/card.ts
@@ -1,5 +1,15 @@
 import tw, { styled } from "twin.macro"
 
-export const Card = styled.article`
-  ${tw`bg-gray-700 rounded-default`}
+export const Card = styled.article<CardProps>`
+  ${tw`block bg-gray-700 rounded-default`}
 `
+
+export type CardProps = {
+  title?: string
+  //if shown as a
+  target?: string
+  rel?: string
+  href?: string
+  // if shown as gatsby-link
+  to?: string
+}
