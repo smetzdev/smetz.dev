8e7cb074d4cde891be936d7599a48b6d81194521
smetzdev <metz@raum3.de>
2021-06-13 23:27:15 +0200

    feat(pages/404): add new design

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,16 +1,14 @@
 import { Link } from "gatsby"
 import "twin.macro"
-import { Gravatar, Layout, SEO } from "../gatsby-components"
+import { ModuleHeadline } from "../elements"
+import { Layout, SEO } from "../gatsby-components"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="404 - Page not found" />
     <div tw="w-screen h-screen flex flex-col items-center justify-center text-center">
-      <h1>
-        <Gravatar tw="w-32 h-auto" />
-      </h1>
+      <ModuleHeadline>404 - Page not found</ModuleHeadline>
       <p tw="mt-4">
-        Page not found...{" "}
         <Link tw="hover:text-primary transition-colors underline" to="/">
           Go to Homepage
         </Link>
