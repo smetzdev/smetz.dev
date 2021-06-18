9c032d6c3a846cefdca50047769842ca124a8ee3
smetzdev <metz@raum3.de>
2021-06-18 12:47:16 +0200

    feat(gravatar): shrink image size

--- src/gatsby-components/gravatar/gravatar.tsx
+++ src/gatsby-components/gravatar/gravatar.tsx
@@ -3,7 +3,7 @@ import "twin.macro"
 
 // https://de.gravatar.com/site/implement/images/
 const gravatarURL =
-  "https://www.gravatar.com/avatar/fb6a9efd248314aad6fece7135ecfe0d?size=500"
+  "https://www.gravatar.com/avatar/fb6a9efd248314aad6fece7135ecfe0d?size=250"
 
 export const Gravatar = ({ className }: GravatarProps) => {
   return (
